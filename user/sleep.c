bfbea3441edad3a52fb8d33f87db39d266e22cf2
younghojan <younghojann@gmail.com>
2023-11-24 15:13:24 +0800

    Fixed Lab utilities: sleep by implementing sleep.c and adding it to UPROGS in Makefile.

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int i;
+    if (argc < 2)
+    {
+        fprintf(2, "usage: sleep ticks...\n");
+        exit(1);
+    }
+    for (i = 1; i < argc; i++)
+        sleep(atoi(argv[i]));
+    exit(0);
+}
